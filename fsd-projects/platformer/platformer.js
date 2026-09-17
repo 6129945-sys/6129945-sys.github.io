ef868e43d83ab6cfad88c8b87d8fa7723938d895
6129945-sys <6129945@opsb.info>
2026-09-17 13:49:42 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,38 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+createPlatform(400, 295, 20, 150, "black")
+createPlatform(0, 650, 250, 20, "red")
+createPlatform(455, 600, 100, 20, "red")
+createPlatform(755, 525, 100, 20, "red")
+createPlatform(700, 238, 20, 150, "black")
+createPlatform(1000, 575, 100, 20, "red")
+createPlatform(980, 180, 20, 250, "black")
+createPlatform(1150, 550, 100, 20, "red")
+createPlatform(1250, 300, 20, 700, "black")
+createPlatform(1000, 420, 100, 20, "red")
+createPlatform(1150, 300, 100, 20, "red")
+createPlatform(1250, 300, 150, 20, "red")
+createPlatform(980, 100, 20, 150, "black")
+createPlatform(1100, 675, 151, 20, "red")
     // TODO 3 - Create Collectables
-
+createCollectable("database", 490, 565);
+createCollectable("database", 1025, 540);
+createCollectable("database", 1185, 640);
+createCollectable("database", 1325,255);
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("left", 675, 0)
+createCannon("top", 200, 700)
+createCannon("top", 675, 1000)
+createCannon("top", 1180, 2000)
+createCannon("left", 300, 2550)
 
     
     
